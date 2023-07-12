31777881057f39f2f23468e229e23b5527aacdfe
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-07-12 23:49:00 +0200

    Added lombok support.

--- src/main/java/edu/charlesmoswane/ecommerce/entity/ProductCategory.java
+++ src/main/java/edu/charlesmoswane/ecommerce/entity/ProductCategory.java
@@ -2,8 +2,12 @@ package edu.charlesmoswane.ecommerce.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "product_category")
+@Getter
+@Setter
 public class ProductCategory {
 }
