d6c3e65d4c37bc0de091d980d52e0988b4e87a86
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-07-13 23:37:34 +0200

    Added attributes for ProductCategory.

--- src/main/java/edu/charlesmoswane/ecommerce/entity/ProductCategory.java
+++ src/main/java/edu/charlesmoswane/ecommerce/entity/ProductCategory.java
@@ -5,9 +5,14 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Entity
 @Table(name = "product_category")
 @Getter
 @Setter
 public class ProductCategory {
+    private Long id;
+    private String categoryName;
+    private Set<Product> products;
 }
