054494b0a178abbfb24d2e6269785e3fadfb05c5
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-07-15 10:29:32 +0200

    Interfaced ProductRepostiroy with JpaRepository.

--- src/main/java/edu/charlesmoswane/ecommerce/dao/ProductRepository.java
+++ src/main/java/edu/charlesmoswane/ecommerce/dao/ProductRepository.java
@@ -1,4 +1,7 @@
 package edu.charlesmoswane.ecommerce.dao;
 
-public interface ProductRepository {
+import edu.charlesmoswane.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
