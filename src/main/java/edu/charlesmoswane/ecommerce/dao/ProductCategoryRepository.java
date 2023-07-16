75f0cc661024215f557582425a3964261815372e
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-07-16 23:50:11 +0200

    Added JPA annotation.

--- src/main/java/edu/charlesmoswane/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/edu/charlesmoswane/ecommerce/dao/ProductCategoryRepository.java
@@ -1,4 +1,7 @@
 package edu.charlesmoswane.ecommerce.dao;
 
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository {
 }
