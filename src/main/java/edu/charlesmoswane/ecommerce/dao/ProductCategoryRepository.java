c29d4e50599e278e1e54725bca1a6d7c85580c9a
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-07-17 22:47:59 +0200

    Set ProductCategoryRepository to extend JpaRepository.

--- src/main/java/edu/charlesmoswane/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/edu/charlesmoswane/ecommerce/dao/ProductCategoryRepository.java
@@ -1,7 +1,9 @@
 package edu.charlesmoswane.ecommerce.dao;
 
+import edu.charlesmoswane.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
-public interface ProductCategoryRepository {
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
 }
