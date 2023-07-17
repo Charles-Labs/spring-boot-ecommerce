d3b9190318c7b620b511705ce0637828da7644e8
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-07-17 23:04:25 +0200

    Updated the mapping.

--- src/main/java/edu/charlesmoswane/ecommerce/entity/ProductCategory.java
+++ src/main/java/edu/charlesmoswane/ecommerce/entity/ProductCategory.java
@@ -19,6 +19,6 @@ public class ProductCategory {
     @Column(name = "category_name")
     private String categoryName;
 
-    @OneToMany(cascade = CascadeType.ALL,mappedBy = "product")
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "category")
     private Set<Product> products;
 }
