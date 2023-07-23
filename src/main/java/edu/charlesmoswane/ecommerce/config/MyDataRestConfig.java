036c23d98f1be33c74d64d9ec71da4961b5da5c7
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-07-23 19:54:06 +0200

    Set the class to a configuration class.

--- src/main/java/edu/charlesmoswane/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/edu/charlesmoswane/ecommerce/config/MyDataRestConfig.java
@@ -2,11 +2,13 @@ package edu.charlesmoswane.ecommerce.config;
 
 import edu.charlesmoswane.ecommerce.entity.Product;
 import edu.charlesmoswane.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
