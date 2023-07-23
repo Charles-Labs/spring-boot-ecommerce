9cf6aaad5987a4ea3a70024afa449521abf573f6
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-07-23 19:53:10 +0200

    Disabled the unsupported actions for the ProductCategory api.

--- src/main/java/edu/charlesmoswane/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/edu/charlesmoswane/ecommerce/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package edu.charlesmoswane.ecommerce.config;
 
 import edu.charlesmoswane.ecommerce.entity.Product;
+import edu.charlesmoswane.ecommerce.entity.ProductCategory;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
@@ -16,6 +17,11 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     }
 }
