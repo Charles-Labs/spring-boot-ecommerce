8b4c4d77279bded4908461289e06583339d72dfe
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-11-24 17:33:53 +0200

    feat: Add lombok and set country as a jpa entity

--- src/main/java/edu/charlesmoswane/ecommerce/entity/Country.java
+++ src/main/java/edu/charlesmoswane/ecommerce/entity/Country.java
@@ -1,9 +1,17 @@
 package edu.charlesmoswane.ecommerce.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "country")
+@Getter
+@Setter
 public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
 }
