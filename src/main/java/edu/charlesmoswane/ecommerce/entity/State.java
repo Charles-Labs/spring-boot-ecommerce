c9178e4228b1e13872e1fa6a4aa56cf97b44c6f1
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-11-28 20:26:29 +0200

    feat: Add attributes to state entity

--- src/main/java/edu/charlesmoswane/ecommerce/entity/State.java
+++ src/main/java/edu/charlesmoswane/ecommerce/entity/State.java
@@ -1,9 +1,22 @@
 package edu.charlesmoswane.ecommerce.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "state")
+@Data
 public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
 }
