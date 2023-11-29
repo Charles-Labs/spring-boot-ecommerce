5f0e01030cb30323dc5d3c1004f1f1b435b360aa
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-11-29 17:44:03 +0200

    feat: Add one-to-many mapping to provinces

--- src/main/java/edu/charlesmoswane/ecommerce/entity/Country.java
+++ src/main/java/edu/charlesmoswane/ecommerce/entity/Country.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Entity
 @Table(name = "country")
 @Getter
@@ -20,8 +22,7 @@ public class Country {
     @Column(name = "name")
     private String name;
 
-
-//    TODO: Add one-to-many mapping to states
-
+    @OneToMany(mappedBy = "country")
+    private Set<State> states;
 
 }
