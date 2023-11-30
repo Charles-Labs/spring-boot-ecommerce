63a4d37344679ece2337de97014cabe0c452792c
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-11-30 18:39:15 +0200

    feat: Add cross origin support to country and state repo

--- src/main/java/edu/charlesmoswane/ecommerce/dao/CountryRepository.java
+++ src/main/java/edu/charlesmoswane/ecommerce/dao/CountryRepository.java
@@ -2,6 +2,8 @@ package edu.charlesmoswane.ecommerce.dao;
 
 import edu.charlesmoswane.ecommerce.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface CountryRepository extends JpaRepository<Country, Long> {
 }
--- src/main/java/edu/charlesmoswane/ecommerce/dao/StateRepository.java
+++ src/main/java/edu/charlesmoswane/ecommerce/dao/StateRepository.java
@@ -2,6 +2,8 @@ package edu.charlesmoswane.ecommerce.dao;
 
 import edu.charlesmoswane.ecommerce.entity.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface StateRepository extends JpaRepository<State, Integer> {
 }
