2184c268aa83ffbd4d776c2374a45aed6fbeeff4
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-12-01 17:52:39 +0200

    feat: Expose country and state repo api's

--- src/main/java/edu/charlesmoswane/ecommerce/dao/CountryRepository.java
+++ src/main/java/edu/charlesmoswane/ecommerce/dao/CountryRepository.java
@@ -2,8 +2,10 @@ package edu.charlesmoswane.ecommerce.dao;
 
 import edu.charlesmoswane.ecommerce.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
 public interface CountryRepository extends JpaRepository<Country, Long> {
 }
