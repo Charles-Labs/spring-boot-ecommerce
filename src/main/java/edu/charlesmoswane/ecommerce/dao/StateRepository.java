b8f3b840cc9765a81679c53a3720a2005f9b212a
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2023-12-01 17:46:53 +0200

    fix: Add search by country code to state repo

--- src/main/java/edu/charlesmoswane/ecommerce/dao/StateRepository.java
+++ src/main/java/edu/charlesmoswane/ecommerce/dao/StateRepository.java
@@ -2,9 +2,12 @@ package edu.charlesmoswane.ecommerce.dao;
 
 import edu.charlesmoswane.ecommerce.entity.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.Set;
 
 @CrossOrigin("http://localhost:4200")
 public interface StateRepository extends JpaRepository<State, Integer> {
-    State findByCode(String code);
+    Set<State> findByCountryCode(@Param("code") String code);
 }
